e6e1d74ca99cefa18e9b46d5a6cf1e118025fb93
Denzel <jdenzelmab@gmail.com>
2023-06-30 18:40:53 -0400

    Made the card css

--- src/Card.js
+++ src/Card.js
@@ -3,16 +3,18 @@ import React from "react";
 function Card({ homes }) {
     return (
         <div  className="card">
-        {homes.map((home, index) => (
+          <div className="card-container">
+          {homes.map((home, index) => (
         <div key={index}>
           <h1>{home.county}</h1>
           <img src={home.image} alt="home"  />
           <h1>{home.type}</h1>
           <p>{home.beds} Beds</p>
           <p>{home.bathrooms} Baths</p>
-          
         </div>
       ))}
+           </div>
+        
         </div>
     );
 }
