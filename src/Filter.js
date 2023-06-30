4772be7e34cd28df71a7229f73d8f89b5f64e073
Denzel <jdenzelmab@gmail.com>
2023-06-30 19:21:09 -0400

    Edited the filter component so that it returns content

---
+++ src/Filter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Filter({ homes }) {
+    return (
+        <div>
+            <h1>Filter</h1>
+            <p>Filter page body content</p>
+        </div>
+    );
+}
+
+export default Filter;
