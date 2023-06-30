86ee311233705deab65f6de3930c17ab3bbc5a91
Denzel <jdenzelmab@gmail.com>
2023-06-29 21:45:00 -0400

    Added 2 components title Home.js and Listings.js

---
+++ src/Listings.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Listings() {
+
+    return (
+        <div>
+            <h1>Listings</h1>
+            <p>Listings page body content</p>
+        </div>
+    );
+}
+
+export default Listings;
