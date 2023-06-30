715863b592042441d98cfb5ed0ab6c1f499105cf
Denzel <jdenzelmab@gmail.com>
2023-06-30 18:30:09 -0400

    Added an image to the card

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,15 @@ import React from "react";
 
 function Card({ homes }) {
     return (
-        <div>
+        <div  className="card">
         {homes.map((home, index) => (
         <div key={index}>
+          <h1>{home.county}</h1>
+          <img src={home.image} alt="home"  />
           <h1>{home.type}</h1>
           <p>{home.beds} Beds</p>
           <p>{home.bathrooms} Baths</p>
+          
         </div>
       ))}
         </div>
