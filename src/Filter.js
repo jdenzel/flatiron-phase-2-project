12416e0689cf52d78ca434e12c9587e6d65c4070
Denzel <jdenzelmab@gmail.com>
2023-07-02 23:17:19 -0400

    Deleted unused lines of code

--- src/Filter.js
+++ src/Filter.js
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-
 function Filter({ homes, onFilterChange }) {
 
     const [county, setCounty] = useState('All');
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import Video from './Video';
 
 function Home() {
